443e9bce3eeb9b796cebaf0730f80fcb365e36b6
s9hn <tnghqn@naver.com>
2022-04-15 23:32:43 +0900

    [Feat] 2주차 과제 진행
    1. 뷰 그리는중

--- app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
@@ -18,7 +18,7 @@ class HomeActivity : AppCompatActivity() {
 
     private fun dataBinding() {
         binding.home = HomeData(
-            "s",
+            "ss",
             "25",
             "ENFJ",
             "우하하하",
