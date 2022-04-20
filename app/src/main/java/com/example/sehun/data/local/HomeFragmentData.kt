db8ceb7d6a2fe136a8af9fe46f457ff39cc5e6ef
s9hn <tnghqn@naver.com>
2022-04-21 03:54:18 +0900

    [Feat] 2주차 과제 진행
    1. 뷰 그리는중

--- app/src/main/java/com/example/sehun/data/local/HomeFragmentData.kt
+++ app/src/main/java/com/example/sehun/data/local/HomeFragmentData.kt
@@ -1,5 +1,6 @@
 package com.example.sehun.data.local
 
 data class HomeFragmentData(
-
+    val name: String,
+    val introduce: String
 )
---
+++ app/src/main/java/com/example/sehun/feature/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sehun.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sehun.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
