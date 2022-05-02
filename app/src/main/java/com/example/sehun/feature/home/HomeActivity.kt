7e63d40185eecec9433c08b8d60e138d2141e961
s9hn <tnghqn@naver.com>
2022-05-03 03:57:03 +0900

    [Feat] 3주차 필수 과제
    1. 로그인뷰(SignIn) 레이아웃 완료
    2. 회원가입뷰(SignUp) 레이아웃 완료
    3. 홈뷰(HomeActivity) 바텀네비게이션 및 탭레이아웃 구현 중

--- app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
@@ -2,53 +2,25 @@ package com.example.sehun.feature.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.example.sehun.R
-import com.example.sehun.data.local.HomeData
 import com.example.sehun.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        dataBinding()
-        initTransactionEvent()
+        initAdapter()
     }
 
-    private fun initTransactionEvent() {
-        val followerFragment = FollowerFragment()
-        val repositoryFragment = RepositoryFragment()
+    private fun initAdapter() {
+        val fragmentList = listOf(ProfileFragment(), HomeFragment(), CameraFragment())
+        viewPagerAdapter = ViewPagerAdapter(this)
+        viewPagerAdapter.fragments.addAll(fragmentList)
 
-        supportFragmentManager.beginTransaction().add(R.id.fcv_home_swaplist, followerFragment)
-            .commit()
-
-        with(binding) {
-            btnHomeFollowerlist.setOnClickListener {
-                supportFragmentManager.beginTransaction().replace(
-                    R.id.fcv_home_swaplist,
-                    followerFragment
-                ).commit()
-            }
-
-            btnHomeRepositorylist.setOnClickListener {
-                supportFragmentManager.beginTransaction().replace(
-                    R.id.fcv_home_swaplist,
-                    repositoryFragment
-                ).commit()
-            }
-        }
-    }
-
-    private fun dataBinding() {
-        binding.home = HomeData(
-            "김세훈",
-            "25",
-            "ENFJ",
-            "우하하\n".repeat(5),
-            R.drawable.ic_launcher_foreground
-        )
+        binding.vpHomeViewpager2.adapter = viewPagerAdapter
     }
 }
--- app/src/main/java/com/example/sehun/feature/home/ProfileFragment.kt
+++ app/src/main/java/com/example/sehun/feature/home/ProfileFragment.kt
@@ -1,60 +1,60 @@
 package com.example.sehun.feature.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.sehun.R
+import com.example.sehun.data.local.HomeData
+import com.example.sehun.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding ?: error("Binding이 초기화 되지 않았습니다.")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        initTransactionEvent()
+        dataBinding()
+        return binding.root
+    }
+
+    private fun initTransactionEvent() {
+        val followerFragment = FollowerFragment()
+        val repositoryFragment = RepositoryFragment()
+
+        childFragmentManager.beginTransaction().add(R.id.fcv_home_swaplist, followerFragment)
+            .commit()
+
+        with(binding) {
+            btnHomeFollowerlist.setOnClickListener {
+                childFragmentManager.beginTransaction().replace(
+                    R.id.fcv_home_swaplist,
+                    followerFragment
+                ).commit()
+            }
+
+            btnHomeRepositorylist.setOnClickListener {
+                childFragmentManager.beginTransaction().replace(
+                    R.id.fcv_home_swaplist,
+                    repositoryFragment
+                ).commit()
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun dataBinding() {
+        binding.home = HomeData(
+            "김세훈",
+            "25",
+            "ENFJ",
+            "우하하\n".repeat(5),
+            R.drawable.ic_launcher_foreground
+        )
     }
 }
