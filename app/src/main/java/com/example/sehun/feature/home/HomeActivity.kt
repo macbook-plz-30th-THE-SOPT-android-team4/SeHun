303440660b18b8e5f344ac81fa9cc8bd4955c231
s9hn <tnghqn@naver.com>
2022-05-03 21:20:53 +0900

    [Feat] 3주차 필수 과제
    1. 로그인뷰(SignIn) 레이아웃 완료
    2. 회원가입뷰(SignUp) 레이아웃 완료
    3. 홈뷰(HomeActivity) 바텀네비게이션 완료 및 레이아웃 구현중

--- app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/sehun/feature/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.sehun.feature.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.sehun.R
 import com.example.sehun.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,8 +14,8 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initAdapter()
+        initBottomNavi()
     }
 
     private fun initAdapter() {
@@ -22,5 +24,39 @@ class HomeActivity : AppCompatActivity() {
         viewPagerAdapter.fragments.addAll(fragmentList)
 
         binding.vpHomeViewpager2.adapter = viewPagerAdapter
+    }
+
+    private fun initBottomNavi() {
+        with(binding) {
+            vpHomeViewpager2.registerOnPageChangeCallback(object :
+                    ViewPager2.OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        bnvHome.menu.getItem(position).isChecked = true
+                    }
+                })
+
+            bnvHome.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_profile -> {
+                        vpHomeViewpager2.currentItem = FIRST_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.menu_profile -> {
+                        vpHomeViewpager2.currentItem = SECOND_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+                    else -> {
+                        vpHomeViewpager2.currentItem = THIRD_FRAGMENT
+                        return@setOnItemSelectedListener true
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val FIRST_FRAGMENT = 0
+        const val SECOND_FRAGMENT = 1
+        const val THIRD_FRAGMENT = 2
     }
 }
