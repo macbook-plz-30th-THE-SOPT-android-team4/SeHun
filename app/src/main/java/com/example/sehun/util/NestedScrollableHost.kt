8f38f4d09b6522e40b4b991b1c61b3d1216ee8e8
s9hn <tnghqn@naver.com>
2022-05-15 00:00:12 +0900

    [Chore]README추가

--- app/src/main/java/com/example/sehun/util/NestedScrollableHost.kt
+++ app/src/main/java/com/example/sehun/util/NestedScrollableHost.kt
@@ -15,7 +15,7 @@ class NestedScrollableHost : FrameLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
-    private var touchSlop =  0
+    private var touchSlop = 0
     private var initialX = 0f
     private var initialY = 0f
     private val parentViewPager: ViewPager2?
