e6369e1ca2acdd39a6d247fb92c8afaa56e73448
s9hn <tnghqn@naver.com>
2022-06-06 23:45:16 +0900

    [Feat] 7차과제ㅂ

--- app/src/main/java/com/example/sehun/feature/home/profile/SettingActivity.kt
+++ app/src/main/java/com/example/sehun/feature/home/profile/SettingActivity.kt
@@ -1,7 +1,7 @@
 package com.example.sehun.feature.home.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sehun.R
 
 class SettingActivity : AppCompatActivity() {
