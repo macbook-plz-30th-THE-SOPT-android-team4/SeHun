4230bd5d811dfb1b7601df930917d482cd907ae5
s9hn <tnghqn@naver.com>
2022-07-02 01:05:46 +0900

    [Feat] 7차과제

--- app/src/main/java/com/example/sehun/feature/home/profile/SettingActivity.kt
+++ app/src/main/java/com/example/sehun/feature/home/profile/SettingActivity.kt
@@ -8,5 +8,6 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
     }
 }
